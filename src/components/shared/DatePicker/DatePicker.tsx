2fe0ff2e84284632f528f17cc27ca34d9e965d65
uiux@okdollar <uiux@okdollar.com>
2025-06-02 17:26:16 +0630

    fix date

--- src/components/shared/DatePicker/DatePicker.tsx
+++ src/components/shared/DatePicker/DatePicker.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import * as React from "react";
-import { format } from "date-fns";
+// import { format } from "date-fns";
 import { CalendarIcon } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
+// import { Calendar } from "@/components/ui/calendar";
 import {
   Popover,
   PopoverContent,
@@ -30,16 +30,16 @@ export function DatePicker({ date, setDate }: Props) {
           )}
         >
           <CalendarIcon />
-          {date ? format(date, "PPP") : <span>Pick a date</span>}
+          {/* {date ? format(date, "PPP") : <span>Pick a date</span>} */}
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-auto p-0" align="start">
-        <Calendar
+        {/* <Calendar
           mode="single"
           selected={date}
           onSelect={setDate}
           initialFocus
-        />
+        /> */}
       </PopoverContent>
     </Popover>
   );
